ea1c40e21d9cc4fe96855ff78a4a394701a544ba
rushikesh pachpute <rushikeshpachpute13@gmail.com>
2024-03-27 16:14:48 +0530

    changed name infooter

--- footer.php
+++ footer.php
@@ -8,7 +8,7 @@
             <li class="active"><a href="home.php"><i class="lni lni-home"></i>Home</a></li>            
             <li><a href="my_classroom.php"><i class="lni lni-shopping-basket"></i>My Classroom</a></li>
 			<li><a href="my_assignment.php"><i class="lni lni-life-ring"></i>My Assignment</a></li>
-            <li><a href="my_meet.php"><i class="lni lni-heart"></i>Google Meets</a></li>
+            <li><a href="my_meet.php"><i class="lni lni-heart"></i>Meets</a></li>
             <li><a href="settings.php"><i class="lni lni-cog"></i>Settings</a></li>
           </ul>
         </div>
